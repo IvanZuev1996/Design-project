15d5c5c762cb1362d00aeef763ced25050914fa9
IvanZuev1996 <93051173+IvanZuev1996@users.noreply.github.com>
2023-04-19 18:57:17 +0300

    modify main page

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,30 +1,36 @@
 import { memo } from 'react';
 import { Page } from 'widgets/Page/Page';
-import Image1 from 'shared/assets/icons/design1.svg'
+import Image1 from 'shared/assets/icons/design1.svg';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
-import cls from './MainPage.module.scss'
+import cls from './MainPage.module.scss';
 
 const MainPage = memo(() => {
     const f = 1;
     return (
         <Page className={cls.wrapper}>
             <div className={cls.contentCard}>
-                <div>
-                    <div className={cls.text}>
+                <div className={cls.textBlock}>
+                    <p className={cls.logoText}>OurDesign</p>
+                    <p className={cls.text}>
                         Мы помогаем зарабатывать в интернете
-                    </div>
+                    </p>
                     <Button theme={ButtonTheme.OUTLINE} className={cls.button}>
                         Обсудить проект
                     </Button>
+                    <div className={cls.paginationDots}>
+                        <span className={cls.active} />
+                        <span />
+                        <span />
+                    </div>
                 </div>
                 <div className={cls.imageWrapper}>
                     <p className={cls.numberOfImage}>01.</p>
                     <p className={cls.textOnImage}>Графический дизайн</p>
-                    <Image1 className={cls.image}/>
+                    <Image1 className={cls.image} />
                 </div>
             </div>
         </Page>
-    )
+    );
 });
 
 export default MainPage;
