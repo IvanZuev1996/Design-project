4284cf26cc5a39dea04134b5336dc5090b3b238b
Ivan Zuev <ivan-zuev-97@mail.ru>
2023-05-14 02:43:52 +0300

    sanity

---
+++ backend_sanity/schemas/index.ts
@@ -0,0 +1,19 @@
+import testimonials from './testimonials'
+import services from './abouts'
+import brands from './brands'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
+
+export const schemaTypes = [
+    testimonials,
+    services,
+    brands,
+    contact,
+    experiences,
+    skills,
+    workExperience,
+    works
+]
---
+++ backend_sanity/schemas/works.ts
@@ -0,0 +1,50 @@
+export default {
+    name: 'works',
+    title: 'Works',
+    type: 'document',
+    fields: [
+      {
+        name: 'id',
+        title: 'Id',
+        type: 'string',
+      },
+
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+    
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'string',
+      },
+      {
+        name: 'figmaLink',
+        title: 'Figma Link',
+        type: 'string',
+      },
+      {
+        name: 'imgUrl',
+        title: 'ImageUrl',
+        type: 'image',
+        options: {
+          hotspot: true,
+        },
+      },
+      {
+        name: 'tags',
+        title: 'Tags',
+       type:'array',
+       of: [
+         {
+           name:'tag',
+           title:'Tag',
+           type:'string'
+         }
+       ]
+      },
+     
+    ],
+  };
